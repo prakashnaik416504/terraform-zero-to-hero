2facba8e8ac66fdfcda14381a9365fdf6168431c
prakashnaik416504 <prakash.100daysofcode@gmail.com>
2025-09-07 16:04:04 +0000

    new file:   Test/.terraform.lock.hcl 	new file:   Test/.terraform/providers/registry.terraform.io/hashicorp/aws/6.12.0/linux_amd64/LICENSE.txt 	new file:   Test/.terraform/providers/registry.terraform.io/hashicorp/aws/6.12.0/linux_amd64/terraform-provider-aws_v6.12.0_x5 	new file:   Test/app.py 	new file:   Test/install.sh 	new file:   Test/main.tf 	new file:   Test/terraform.tfstate 	new file:   Test/terraform.tfstate.backup 	new file:   Test/terraform.tfvars 	new file:   Test/variables.tf 	new file:   terraform.tfstate

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
+  ami_value = "ami-0af9569868786b23a" # replace this
   instance_type_value = "t2.micro"
   subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
 }
